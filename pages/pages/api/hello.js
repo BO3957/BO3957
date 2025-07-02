208b1c9d4bc2f66ae35fd5cdfecf7d1c24291f2f
BO3957 <bokyunglee9903@gmail.com>
2025-07-02 19:26:40 +0900

    Update hello.js

--- pages/pages/api/hello.js
+++ pages/pages/api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from WoojinChemical API!" });
+  res.status(200).json({ message: 'Hello from WoojinChemical API!' });
 }
